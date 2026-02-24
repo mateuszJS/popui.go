4079ad61ae064de96e46e029d9c12d323bf503d9
Mark MacKay <mark@method.ac>
2026-02-24 20:05:36 +0100

    remove the icon generate script because now we copy and paste from the icons repo

--- icons/icons.go
+++ icons/icons.go
@@ -1,5 +1,2 @@
-// Package icons provides easy access to all of the icons provided by popui
-// including a generator script for helper methods.
+// Package icons provides easy access to all of the icons provided by popui.
 package icons
-
-//go:generate go run generate.go
